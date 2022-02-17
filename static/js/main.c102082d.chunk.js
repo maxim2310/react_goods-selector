9cc22d56b27e00d88e80233393de609018f2c6fc
maxim2310 <m23a09x96@gmail.com>
2022-02-17 09:19:23 +0200

    Updates

---
+++ static/js/main.c102082d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact_goods-selector"]=this["webpackJsonpreact_goods-selector"]||[]).push([[0],{10:function(e,s,t){"use strict";t.r(s);var c=t(3),o=t.n(c),r=(t(1),t(8),t(0)),n=["Dumplings","Carrot","Eggs","Ice cream","Apple","Bread","Fish","Honey","Jam","Garlic"],a=function(){return Object(r.jsxs)("div",{className:"App",children:[Object(r.jsx)("h1",{children:"No goods selected"}),n.length]})};o.a.render(Object(r.jsx)(a,{}),document.getElementById("root"))},8:function(e,s,t){}},[[10,1,2]]]);
+//# sourceMappingURL=main.c102082d.chunk.js.map
